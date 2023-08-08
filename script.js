825736ce84521fe5898a5e045282ba225c257aa0
muazzamgujjar <muazzamgujjarmuazzam@gmail.com>
2023-08-08 05:57:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const changeBodyRed =( )=>{
+    document.body.firstElementChild.computedStyleMap.background="red"
+}
+
+let b = document.body
+console.log("First child of b is : ",b.firstChild)
+console.log("First child of b is : ",b.firstElementChild)
